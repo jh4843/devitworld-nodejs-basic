c84fb633974b9dae83f80b84748b5854e63de732
jh4843 <creamboy1@naver.com>
2024-05-29 23:37:22 +0900

    02_module_npm

---
+++ 02_01_commonjs_module/app.js
@@ -0,0 +1,4 @@
+const add = require("./module_math");
+
+const result = add(2, 3);
+console.log(result); // 5
---
+++ 02_02_es6_module copy/app.js
@@ -0,0 +1,5 @@
+// app.mjs
+import { add } from "./module_math.mjs";
+
+const result = add(2, 3);
+console.log(result); // 출력: 5
